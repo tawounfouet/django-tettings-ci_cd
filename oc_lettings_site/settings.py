2fd5c358eafddc8c418a25b8b111f04757473093
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2025-07-14 20:00:34 +0200

    update secrets

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! test GH_SECRET_KEY
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
